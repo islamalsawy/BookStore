a9d34227f0696df11fb35ae690721974f3e8f1b2
IsLam Alsawy <esl14@yahoo.com>
2019-08-29 02:01:36 +0200

    Adding Dependancy Container

---
+++ BookStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BookStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BookStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1c621a8f-801b-49e3-8c3c-c8c39ada5c95")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BookStoreDomain/Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreDomain.Entities
+{
+    class Book
+    {
+        public int ISBN { get; set; }
+        public string Title { get; set; }
+        public int Author { get; set; }
+        public string Description { get; set; }
+        public int Category { get; set; }    
+        public int Price { get; set; }
+
+
+
+    }
+}
