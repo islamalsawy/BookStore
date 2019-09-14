ebc62c78a6aaaaa13659c0ba10880915da77771e
IsLam Alsawy <esl14@yahoo.com>
2019-09-14 22:36:19 +0200

    Adding HTML Helper Method- PageLinks

---
+++ BookStoreWebUI/HTMLHelper/PagingHelper.cs
@@ -0,0 +1,33 @@
+﻿using BookStoreWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStoreWebUI.HTMLHelper
+{
+    public static class PagingHelper
+    {
+        public MvcHtmlString PageLinks(
+            this HtmlHelper html,
+            PagingInfo pageInfo,
+            Func<int,string> pageURL)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 1; i < pageInfo.TotalPage; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageURL(i));
+                tag.InnerHtml = i.ToString();
+                if (i==pageInfo.CurrentPage)
+                {
+                    tag.AddCssClass("Selected");
+                    tag.AddCssClass("btn-Primary");
+                }
+                tag.AddCssClass("btn btn-default");
+            }
+        }
+    }
+}
--- BookStoreWebUI/Models/PagingInfo.cs
+++ BookStoreWebUI/Models/PagingInfo.cs
@@ -7,5 +7,15 @@ namespace BookStoreWebUI.Models
 {
     public class PagingInfo
     {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPage {
+            get
+            {
+                return (int) Math.Ceiling((decimal)TotalItems / ItemsPerPage); 
+            }
+        }
+
     }
 }
